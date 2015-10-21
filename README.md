5da6c7a6885cb4575f5a077ba622e87faedcf9eb
Jotune <Jean-Charbel.VANNIER@supinfo.com>
2015-10-21 23:58:39 +0200

    Update of readme. Added canvas construction

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Ambient Video Projection (AVP)
 Inspired by Phillips Ambilight, this JavaScript plugin creates light effects around any HTML5 video that correspond to the video content, resulting in a more immersive experience.
+
+##How to use
+
+First off all, you need to create a container that will contain the video and the canvas in which we will draw our annimation. This container must be in *relative* or *absolute* position. We recomand that you define his background in black. This is not mandatory.
+
+Then, you just have to init avp_core providing the video's Id and the recently created container's Id.
+
+    var avp_core = new avp_core("html5video", "avpContainer");
+    avp_core.Start();
+	
+The AVP effect will now fill your container.
--- avpcore.js
+++ avpcore.js
@@ -3,12 +3,25 @@
 // https://github.com/Jotune/AmbientVideoLightsProjection_Core
 
 //Core method, you need to supply a <video> element
-function avp_core(video, refreshRate = 30) {
-	var canvasAvp = document.getElementById('AvpCanvas');
-	canvasContainer.style.webkitFilter = "blur(80px)";
+function avp_core(videoId, containerId,  refreshRate = 30) {
+	var video = document.getElementById(videoId);
+	var canvasAvp = CreateCanvas(containerId);	
 	var ctxAvp = canvasAvp.getContext('2d');
 	var annimation;
-
+	
+	var CreateCanvas(containerId) {
+		var canvas = document.createElement("canvas");
+        canvas.style.position = "absolute";
+        document.getElementById(containerId).appendChild(canvas);
+        canvas.id = "AvpCanvas";
+        canvas.style.top = 0;
+        canvas.style.left = 0;
+        canvas.style.width = "100%";
+        canvas.style.height = "100%";
+        canvasContainer.style.webkitFilter = "blur(80px)";
+		return canvas;
+	}
+	
 	// This method will start drawing the video the canvasAvp
 	this.Start = function () {
 		canvasAvp.setAttribute('width', video.videoWidth);
@@ -22,6 +35,8 @@ function avp_core(video, refreshRate = 30) {
 					ctxAvp.drawImage(video, 0, 0);
 				}
 			}, refreshRate);
+			//Start playing the video
+			video.play();
 		}
 	}
 
